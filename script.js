1f0b688abb43d90e5c8f267da4e0b81137fe6db2
Ekwusie Sophia <sophiaekwusie70@gmail.com>
2024-05-12 06:00:04 -0700

    commited

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ const myboxnode= document.querySelector(".mybox");
 const mybuttonnode = document.getElementById("mybutton")
 // console.log(myboxnode)
  function changeColor(event){
-    myboxnode.style.backgroundColor = "yellow";
-    myboxnode.textContent= "Don't do it😟";
+   setTimeout(function(){
+      myboxnode.style.backgroundColor = "yellow";
+      myboxnode.textContent= "Don't do it😟";
+
+   }, 250) 
  }
 mybuttonnode.addEventListener("mouseover", changeColor);
 
 mybuttonnode.addEventListener("mouseout", event => {
-   myboxnode.style.backgroundColor = "aqua"
-   myboxnode.textContent = "Click me🙂";
+   setTimeout(function(){
+      myboxnode.style.backgroundColor = "aqua"
+      myboxnode.textContent = "Click me🙂";
+   },100)  
 })
 
 mybuttonnode.addEventListener("click", function(event){
-   myboxnode.style.backgroundColor = "red"
-   myboxnode.textContent = "OUCH!😱"
+   setTimeout(function(){
+      myboxnode.style.backgroundColor = "red"
+      myboxnode.textContent = "OUCH!😱"
+
+   }, 250)
 })
+
